c1dbb0fdc52eaa50ffed825dce694cc66b576951
mrsMacAlister <75312297+mrsMacAlister@users.noreply.github.com>
2022-04-12 13:05:23 +0200

    debugging cityUpdate, API implementation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 function App() {
-      let weatherData = {
-    city: "Taipei",
-    country: "TW",
-    time: "05:20",
-    day: "Thursday",
-    temperature: 30,
-    description: "Sunny",
-    humidity: 55,
-    wind: 14,
-    imgUrl: "http://openweathermap.org/img/wn/01d@2x.png"
-  };
-  function handleSubmit(response) {
-    return(
-      alert("hello")
-    )
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState([]);
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      description: response.data.weather[0].main,
+      humidity: response.data.main.humidity,
+      wind: Math.round(response.data.wind.speed),
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "4c09ae07987b07a4993b3f7e761af71d";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayWeather);
   }
 
-  function updateCity(response){
-    alert("wazzap")
+  //https://api.openweathermap.org/data/2.5/weather?q=Monaco&appid=4c09ae07987b07a4993b3f7e761af71d&units=metric`;
+
+  function updateCity(event) {
+    setCity(event.target.value);
+    console.log(event.target.value);
   }
 
   return (
@@ -36,6 +41,7 @@ function App() {
                   type="text"
                   placeholder="Enter your city"
                   autoComplete="off"
+                  onChange={updateCity}
                 />
               </div>
               <div id="search-button" className="col-3 form-el">
@@ -43,7 +49,6 @@ function App() {
                   id="search-btn"
                   className="btn btn-warning"
                   type="submit"
-                  onChange={updateCity}
                 >
                   Search
                 </button>
@@ -52,11 +57,11 @@ function App() {
           </form>
           <div className="row information">
             <div className="col-2 weather-icon">
-              <img id="big-icon" src={weatherData.imgUrl} alt="" />
+              <img id="big-icon" src={weather.icon} alt={weather.description} />
             </div>
             <div id="weather-info" className="col-5">
               <h2 id="big-degrees">
-                <span id="current-temp">{weatherData.temperature}</span>
+                <span id="current-temp">{weather.temperature}</span>
                 <span id="temp-units">
                   <span id="celsius" data-ol-has-click-handler="">
                     °C
@@ -69,23 +74,23 @@ function App() {
               </h2>
               <ul id="weather-description">
                 <li>
-                  Weather: <span id="sky">{weatherData.description}</span>
+                  Weather: <span id="sky">{weather.description}</span>
                 </li>
                 <li>
-                  Humidity: <span id="humidity">{weatherData.humidity}</span>%
+                  Humidity: <span id="humidity">{weather.humidity}</span>%
                 </li>
                 <li>
-                  Wind: <span id="wind">{weatherData.wind}</span>{" "}
+                  Wind: <span id="wind">{weather.wind}</span>{" "}
                   <span id="windUnit">km/h</span>
                 </li>
               </ul>
             </div>
             <div className="col-5">
-              <h2>{weatherData.city}</h2>
+              <h2>{weather.city}</h2>
               <ul id="time-date">
-                <li id="country">{weatherData.country}</li>
-                <li id="time">{weatherData.time}</li>
-                <li id="day">{weatherData.day}</li>
+                <li id="country">{weather.country}</li>
+                <li id="time">{weather.time}</li>
+                <li id="day">{weather.day}</li>
               </ul>
             </div>
           </div>
